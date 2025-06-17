91b4724842aad8b5e443298e46522aff1ffa13a4
Alexander Kaschta <alexander.kaschta@t-online.de>
2025-06-17 23:23:53 +0200

    ADD: Style selection

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,55 +1,63 @@
 <script setup lang="ts">
 import 'maplibre-gl/dist/maplibre-gl.css'
-import {Map, NavigationControl} from 'maplibre-gl'
+import {Map, NavigationControl, type StyleSpecification} from 'maplibre-gl'
 import {Marker} from "../ui/marker.ts";
 import {onMounted} from 'vue'
+import {useSettingsStore} from "../store/settings.ts";
+import {colorful} from "@versatiles/style";
 
 let markers = [];
 
-function sleep(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+const settingsStore = useSettingsStore();
 
-async function demo() {
-  for (let i = 0; i < 1000; i++) {
-    markers[0].setLngLat([8 + i / 1000, 48.95])
-    await sleep(1000);
-  }
-  console.log('Done');
-}
+// Define the styles
+const vectorTilesStyle = colorful({
+  baseUrl: 'http://127.0.0.1:8080',
+  language: 'de'
+})
 
+const osmStyle: StyleSpecification = {
+  'version': 8,
+  'name': 'Raster tiles',
+  'center': [0, 0],
+  'zoom': 0,
+  'sources': {
+    'raster-tiles': {
+      'type': 'raster',
+      'tiles': ['https://tile.openstreetmap.org/{z}/{x}/{y}.png'],
+      'tileSize': 256,
+      'minzoom': 0,
+      'maxzoom': 19
+    }
+  },
+  'layers': [
+    {
+      'id': 'background',
+      'type': 'background',
+      'paint': {
+        'background-color': '#e0dfdf'
+      }
+    },
+    {
+      'id': 'simple-tiles',
+      'type': 'raster',
+      'source': 'raster-tiles'
+    }
+  ]
+}
 
 onMounted(() => {
+  const style = settingsStore.useVectorTiles ? vectorTilesStyle : osmStyle
+
   const map = new Map({
     container: 'map',
-    style: 'https://demotiles.maplibre.org/style.json',
-    center: [8, 49],
+    style: style,
+    center: [8.4, 49],
     zoom: 11
   })
 
   let control = new NavigationControl()
   map.addControl(control)
-
-
-  //const ref_marker = new Marker()
-  //    .setLngLat([8, 48.95])
-  //    .addTo(map);
-
-  const marker = new Marker({label: "Test", status: 8})
-      .setLngLat([8, 48.95])
-      .addTo(map);
-
-  markers.push(marker)
-
-  const marker2 = new Marker()
-      .setLngLat([8, 48.951])
-      .addTo(map);
-
-  markers.push(marker2)
-
-  //demo()
-
-
 })
 </script>
 
