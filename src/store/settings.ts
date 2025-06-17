6d6d6fe78f24183d92916c387f89866cd3474cc4
Alexander Kaschta <alexander.kaschta@t-online.de>
2025-06-17 23:09:41 +0200

    ADD: Settings store

---
+++ src/store/settings.ts
@@ -0,0 +1,10 @@
+import { defineStore } from 'pinia'
+import type { Ref } from 'vue'
+import { ref } from 'vue'
+
+export const useSettingsStore = defineStore('settings', () => {
+    const useVectorTiles: Ref<boolean> = ref(true);
+
+    return { useVectorTiles }
+}, { persist: true })
+
