7af66d2cd846048e985462df7345b1e043a33636
Alexander Kaschta <alexander.kaschta9@kit.edu>
2025-07-26 23:51:54 +0200

    UPD: Sort trackers by name

--- src/store/connection.ts
+++ src/store/connection.ts
@@ -22,8 +22,22 @@ export const useConnectionStore = defineStore("connection", () => {
         }).then(function (response) {
             // Reload the data
             markers.value = response.data;
+
+            // Sort trackers according to their name
+            markers.value.sort((a, b) => {
+                let item_a = a.resource == null ? a.name : a.resource.name;
+                let item_b = b.resource == null ? b.name : b.resource.name;
+                if (item_a  < item_b) {
+                    return -1;
+                } else if (item_a > item_b) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            })
+
         }).catch(function (e) {
-            console.log("Cannot get data");
+            console.log(e);
         })
     }
 
