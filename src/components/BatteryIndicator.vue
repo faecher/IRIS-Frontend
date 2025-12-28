abde883e0f4776597c8a4f7bead0cc1fd11fdef8
Alexander Kaschta <alexander.kaschta9@kit.edu>
2025-12-28 23:24:08 +0100

    UPD: Remove external dependencies from battery indicator

--- src/components/BatteryIndicator.vue
+++ src/components/BatteryIndicator.vue
@@ -9,14 +9,30 @@ defineProps({
 
 <template>
 <div class="flex items-center">
-  <img v-if="value < 12.5" src="/battery_0.svg" alt="Battery charge indicator">
-  <img v-else-if="value < 25.0" src="/battery_1.svg" alt="Battery charge indicator">
-  <img v-else-if="value < 37.5" src="/battery_2.svg" alt="Battery charge indicator">
-  <img v-else-if="value < 50.0" src="/battery_3.svg" alt="Battery charge indicator">
-  <img v-else-if="value < 62.5" src="/battery_4.svg" alt="Battery charge indicator">
-  <img v-else-if="value < 75.0" src="/battery_5.svg" alt="Battery charge indicator">
-  <img v-else-if="value < 87.5" src="/battery_6.svg" alt="Battery charge indicator">
-  <img v-else src="/battery_7.svg" alt="Battery charge indicator">
+  <svg v-if="value < 12.5" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#d00000">
+    <path d="M320-80q-17 0-28.5-11.5T280-120v-640q0-17 11.5-28.5T320-800h80v-80h160v80h80q17 0 28.5 11.5T680-760v640q0 17-11.5 28.5T640-80H320Zm40-80h240v-560H360v560Z"/>
+  </svg>
+  <svg v-else-if="value < 25.0" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#d00000">
+    <path d="M320-80q-17 0-28.5-11.5T280-120v-640q0-17 11.5-28.5T320-800h80v-80h160v80h80q17 0 28.5 11.5T680-760v640q0 17-11.5 28.5T640-80H320Zm40-160h240v-480H360v480Z"/>
+  </svg>
+  <svg v-else-if="value < 37.5" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f">
+    <path d="M320-80q-17 0-28.5-11.5T280-120v-640q0-17 11.5-28.5T320-800h80v-80h160v80h80q17 0 28.5 11.5T680-760v640q0 17-11.5 28.5T640-80H320Zm40-240h240v-400H360v400Z"/>
+  </svg>
+  <svg v-else-if="value < 50.0" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f">
+    <path d="M320-80q-17 0-28.5-11.5T280-120v-640q0-17 11.5-28.5T320-800h80v-80h160v80h80q17 0 28.5 11.5T680-760v640q0 17-11.5 28.5T640-80H320Zm40-320h240v-320H360v320Z"/>
+  </svg>
+  <svg v-else-if="value < 62.5" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f">
+    <path d="M320-80q-17 0-28.5-11.5T280-120v-640q0-17 11.5-28.5T320-800h80v-80h160v80h80q17 0 28.5 11.5T680-760v640q0 17-11.5 28.5T640-80H320Zm40-400h240v-240H360v240Z"/>
+  </svg>
+  <svg v-else-if="value < 75.0" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f">
+    <path d="M320-80q-17 0-28.5-11.5T280-120v-640q0-17 11.5-28.5T320-800h80v-80h160v80h80q17 0 28.5 11.5T680-760v640q0 17-11.5 28.5T640-80H320Zm40-480h240v-160H360v160Z"/>
+  </svg>
+  <svg v-else-if="value < 87.5" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f">
+    <path d="M320-80q-17 0-28.5-11.5T280-120v-640q0-17 11.5-28.5T320-800h80v-80h160v80h80q17 0 28.5 11.5T680-760v640q0 17-11.5 28.5T640-80H320Zm40-560h240v-80H360v80Z"/>
+  </svg>
+  <svg v-else xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f">
+    <path d="M320-80q-17 0-28.5-11.5T280-120v-640q0-17 11.5-28.5T320-800h80v-80h160v80h80q17 0 28.5 11.5T680-760v640q0 17-11.5 28.5T640-80H320Z"/>
+  </svg>
   <p class="w-12 text-right select-none">{{ value }} %</p>
 </div>
 </template>
